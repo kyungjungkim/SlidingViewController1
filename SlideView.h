8b7731a1c82014447379e4f393d5168423c94576
kyungjung kim <kj.kim@live.co.kr>
2018-11-08 10:11:23 +0900

    SlideView header file
    initial upload.

---
+++ SlideView.h
@@ -0,0 +1,72 @@
+//
+//  SlideView.h
+//  CustomTableView
+//
+//  Created by kkj on 18/11/08.
+//  Copyright 2018 __kkj__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    BLENDIN,              
+	GROW,
+	BOTH
+} AnimationType;
+
+
+@protocol SlideViewDelegate
+
+@required
+
+- (void)SlideViewCellSelected:(NSInteger)returnIndex;
+
+@end
+
+
+@interface SlideView : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+
+	UITableView *uiTableView;
+	NSArray *arrayData;
+	CGFloat heightOfCell;
+	CGFloat paddingLeft;
+	CGFloat paddingRight;
+	CGFloat paddingTop;
+	CGFloat heightTableView;
+	UIView *refView;
+	
+	id<DropDownViewDelegate> delegate;
+	
+	NSInteger animationType;
+	CGFloat open;
+	CGFloat close;
+}
+
+@property (nonatomic) id<SlideViewDelegate> delegate;
+
+@property (nonatomic, retain)UITableView *uiTableView;
+
+@property (nonatomic, retain) NSArray *arrayData;
+
+@property (nonatomic) CGFloat heightOfCell;
+
+@property (nonatomic) CGFloat paddingLeft;
+
+@property (nonatomic) CGFloat paddingRight;
+
+@property (nonatomic) CGFloat paddingTop;
+
+@property (nonatomic) CGFloat heightTableView;
+
+@property (nonatomic, retain) UIView *refView;
+
+@property (nonatomic) CGFloat open;
+
+@property (nonatomic) CGFloat close;
+
+- (id)initWithArrayData:(NSArray *)data cellHeight:(CGFloat)cHeight heightTableView:(CGFloat)tHeightTableView paddingTop:(CGFloat)tPaddingTop paddingLeft:(CGFloat)tPaddingLeft paddingRight:(CGFloat)tPaddingRight refView:(UIView *)rView animation:(AnimationType)tAnimation openAnimationDuration:(CGFloat)openDuration closeAnimationDuration:(CGFloat)closeDuration;
+
+- (void)closeAnimation;
+- (void)openAnimation;
+
+@end
