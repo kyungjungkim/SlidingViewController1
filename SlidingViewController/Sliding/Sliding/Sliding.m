5f22ee9fcc442e72154a7c861f62650c85269f42
kyungjung kim <kj.kim@live.co.kr>
2020-06-04 20:24:05 +0900

    Add files via upload

--- SlidingViewController/Sliding/Sliding/Sliding.m
+++ SlidingViewController/Sliding/Sliding/Sliding.m
@@ -35,43 +35,43 @@
 //}
 
 // Open the hamburger menu.
-- (void)menuOpenBtnClick {
-    if (isMenuOpen == NO) {
-        [UIView animateWithDuration:0.8f animations:^{
-            [self->menu setFrame:CGRectMake(0.0f, 30.0f, self.bounds.size.width - 100.0f, self.bounds.size.height - 30.0f)];
-        } completion:^(BOOL finished) {
-            if (finished) {
-                self->cell.uiBCheck.frame = CGRectMake(self->cell.uiBCheck.frame.origin.x,
-                                                       self->cell.uiBCheck.frame.origin.y - 10.0f,
-                                                       self->cell.uiBCheck.frame.size.width,
-                                                       self->cell.uiBCheck.frame.size.height);
-                [self->cell.uiBCheck setAlpha:1.0f];
-            }
-        }];
-        
-        [self bringSubviewToFront:menu];
-        
-        self->isMenuOpen = YES;
-    }
-}
-
-- (void)swipeMenuClose {
-    if (isMenuOpen == YES) {
-        [UIView animateWithDuration:0.8f animations:^{
-            [self->menu setFrame:CGRectMake(-(self.bounds.size.width - 100.0f), 30.0f, self.bounds.size.width - 100.0f, self.bounds.size.height - 30.0f)];
-        } completion:^(BOOL finished) {
-            if (finished) {
-                self->cell.uiBCheck.frame = CGRectMake(self->cell.uiBCheck.frame.origin.x,
-                                                       self->cell.uiBCheck.frame.origin.y + 10.0f,
-                                                       self->cell.uiBCheck.frame.size.width,
-                                                       self->cell.uiBCheck.frame.size.height);
-                [self->cell.uiBCheck setAlpha:1.0f];
-                
-                self->isMenuOpen = NO;
-            }
-        }];
-    }
-}
+//- (void)menuOpenBtnClick {
+//    if (isMenuOpen == NO) {
+//        [UIView animateWithDuration:0.8f animations:^{
+//            [self->menu setFrame:CGRectMake(0.0f, 30.0f, self.bounds.size.width - 100.0f, self.bounds.size.height - 30.0f)];
+//        } completion:^(BOOL finished) {
+//            if (finished) {
+//                self->cell.uiBCheck.frame = CGRectMake(self->cell.uiBCheck.frame.origin.x,
+//                                                       self->cell.uiBCheck.frame.origin.y - 10.0f,
+//                                                       self->cell.uiBCheck.frame.size.width,
+//                                                       self->cell.uiBCheck.frame.size.height);
+//                [self->cell.uiBCheck setAlpha:1.0f];
+//            }
+//        }];
+//
+//        [self bringSubviewToFront:menu];
+//
+//        self->isMenuOpen = YES;
+//    }
+//}
+//
+//- (void)swipeMenuClose {
+//    if (isMenuOpen == YES) {
+//        [UIView animateWithDuration:0.8f animations:^{
+//            [self->menu setFrame:CGRectMake(-(self.bounds.size.width - 100.0f), 30.0f, self.bounds.size.width - 100.0f, self.bounds.size.height - 30.0f)];
+//        } completion:^(BOOL finished) {
+//            if (finished) {
+//                self->cell.uiBCheck.frame = CGRectMake(self->cell.uiBCheck.frame.origin.x,
+//                                                       self->cell.uiBCheck.frame.origin.y + 10.0f,
+//                                                       self->cell.uiBCheck.frame.size.width,
+//                                                       self->cell.uiBCheck.frame.size.height);
+//                [self->cell.uiBCheck setAlpha:1.0f];
+//
+//                self->isMenuOpen = NO;
+//            }
+//        }];
+//    }
+//}
 
 
 @end
