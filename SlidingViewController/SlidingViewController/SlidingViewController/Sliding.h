da2c0a5dea26f89cbad91c78f9c74a448ffbe32d
kyungjung kim <kj.kim@live.co.kr>
2020-07-26 10:11:03 +0900

    Add files via upload

---
+++ SlidingViewController/SlidingViewController/SlidingViewController/Sliding.h
@@ -0,0 +1,27 @@
+//
+//  SlidingViewController.h
+//  Sliding
+//
+//  Created by Kyungjung Kim on 2020/07/25.
+//  Copyright © 2020 prosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MenuTable/MenuTableViewCell.h"
+#import "MenuTable/MenuTableViewHeaderViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Sliding : UIViewController <UITableViewDelegate, UITableViewDataSource, HeaderViewCellDelegate, TableViewCellDelegate> {
+    MenuTableViewHeaderViewController *cell;
+    MenuTableViewCell *cell1;
+    long m_tag;
+    
+    BOOL isMenuOpen;
+    
+    UITableView *m_menu;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
